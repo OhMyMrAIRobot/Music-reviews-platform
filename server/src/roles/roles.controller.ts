f2d5813ac98553e19d0e581c7cacc20c041b56c4
Daniil Khlyshchankou <gfhieemobilelegends@gmail.com>
2025-03-09 21:06:44 +0300

    Added: root admin role and protect roles routes

--- server/src/roles/roles.controller.ts
+++ server/src/roles/roles.controller.ts
@@ -6,20 +6,20 @@ import {
   Param,
   Post,
   Put,
+  UseGuards,
 } from '@nestjs/common';
 import { RolesService } from './roles.service';
 import { Role } from '@prisma/client';
 import { CreateRoleDto } from './dto/create-role.dto';
 import { UpdateRoleDto } from './dto/update-role.dto';
+import { Roles } from '../auth/decorator/roles.decorator';
+import { UserRole } from './type/userRole';
+import { RolesGuard } from '../auth/guard/roles.guard';
+import { JwtAuthGuard } from '../auth/guard/jwt-auth.guard';
 
 @Controller('roles')
 export class RolesController {
   constructor(private readonly roleService: RolesService) {}
-
-  @Post()
-  async createRole(@Body() createRoleDto: CreateRoleDto): Promise<Role> {
-    return this.roleService.createRole(createRoleDto);
-  }
 
   @Get()
   async getRoles(): Promise<Role[]> {
@@ -31,6 +31,15 @@ export class RolesController {
     return this.roleService.getRoleById(id);
   }
 
+  @Roles(UserRole.ROOT_ADMIN)
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Post()
+  async createRole(@Body() createRoleDto: CreateRoleDto): Promise<Role> {
+    return this.roleService.createRole(createRoleDto);
+  }
+
+  @Roles(UserRole.ROOT_ADMIN)
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @Put(':id')
   async updateRole(
     @Param('id') id: string,
@@ -39,6 +48,8 @@ export class RolesController {
     return this.roleService.updateRole(id, updateRoleDto);
   }
 
+  @Roles(UserRole.ROOT_ADMIN)
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @Delete(':id')
   async deleteRole(@Param('id') id: string): Promise<Role> {
     return this.roleService.deleteRole(id);
--- server/src/roles/type/userRole.ts
+++ server/src/roles/type/userRole.ts
@@ -2,4 +2,5 @@ export enum UserRole {
   USER = 'User',
   SUPER_USER = 'SuperUser',
   ADMIN = 'Admin',
+  ROOT_ADMIN = 'RootAdmin',
 }
