03e09d8e1d528d7cd589c2b38d7794abddfc1e81
Daniil Khlyshchankou <gfhieemobilelegends@gmail.com>
2025-03-26 16:26:27 +0300

    Fixes: paths in mails

--- server/src/mails/mails.service.ts
+++ server/src/mails/mails.service.ts
@@ -22,21 +22,21 @@ export class MailsService {
   async sendRegistrationEmail(email: string, username: string, token: string) {
     await this.sendEmail(email, 'Добро пожаловать в MyApp!', 'register', {
       username: username,
-      activationLink: `${process.env.FRONTEND}/auth/activate?token=${token}`,
+      activationLink: `${process.env.FRONTEND}/auth/activate/${token}`,
     });
   }
 
   async sendActivationEmail(email: string, username: string, token: string) {
     await this.sendEmail(email, 'Активация аккаунта MyApp!', 'activation', {
       username: username,
-      activationLink: `${process.env.FRONTEND}/auth/activate?token=${token}`,
+      activationLink: `${process.env.FRONTEND}/auth/activate/${token}`,
     });
   }
 
   async sendResetPasswordEmail(email: string, username: string, token: string) {
     await this.sendEmail(email, 'Сброс пароля!', 'reset-password', {
       username: username,
-      resetLink: `${process.env.FRONTEND}/auth/reset?token=${token}`,
+      resetLink: `${process.env.FRONTEND}/auth/reset-password/${token}`,
     });
   }
 
