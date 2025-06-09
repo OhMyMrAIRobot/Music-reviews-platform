efdb3b847e08adf4fae204796992002993dc014e
Daniil Khlyshchankou <gfhieemobilelegends@gmail.com>
2025-06-09 23:07:53 +0300

    Added: most reviewed releases swiper

---
+++ client/src/pages/main-page/ui/most-reviewed-releases/swiper/Most-reviewed-swiper-card.tsx
@@ -0,0 +1,93 @@
+import { FC } from 'react'
+import ReleaseRatings from '../../../../../components/release/Release-ratings'
+import ReleaseTypeIcon from '../../../../../components/release/Release-type-icon'
+import { TextReviewSvgIcon } from '../../../../../components/svg/ReleaseSvgIcons'
+import useCustomNavigate from '../../../../../hooks/UseCustomNavigate'
+import { IRelease } from '../../../../../models/release/Release'
+
+interface IProps {
+	release: IRelease | null
+	index: number
+}
+
+const MostReviewedCarouselCard: FC<IProps> = ({ release, index }) => {
+	const { navigateToRelease } = useCustomNavigate()
+
+	return (
+		release && (
+			<div className='w-full max-w-[300px] lg:max-w-[350px] flex flex-col items-center shadow-2xl select-none border border-white/10 bg-black/40 rounded-[30px] lg:rounded-[40px] p-[15px] overflow-hidden h-[85%]'>
+				{/* COVER */}
+				<div className='flex flex-col items-center gap-2 relative '>
+					<span className='font-semibold absolute left-3 top-3 lg:left-4 lg:top-4 bg-black/10 text-white backdrop-blur-md text-[12px] lg:text-[13px] h-7 rounded-full px-3 flex items-center'>
+						Топ-{index + 1} за сутки на РЗТ
+					</span>
+
+					<ReleaseTypeIcon
+						type={release.release_type}
+						className={
+							'absolute size-9 bottom-4 right-4 bg-zinc-900 rounded-full flex items-center justify-center p-2'
+						}
+					/>
+
+					<img
+						loading='lazy'
+						decoding='async'
+						src={`${import.meta.env.VITE_SERVER_URL}/public/releases/${
+							release.img
+						}`}
+						className='rounded-[15px] lg:rounded-[25px] border border-white/10'
+					/>
+				</div>
+
+				{/* TITLE, AUTHORS, MARKS */}
+				<div className='flex justify-between w-full items-center gap-5 mt-2 '>
+					<div className='w-full'>
+						<span className='text-left lg:text-xl font-medium block'>
+							{release.title}
+						</span>
+						<div className='flex flex-wrap gap-1 font-semibold leading-3 mt-2'>
+							{release.author.map((author, index) => (
+								<div key={author.name} className='flex'>
+									<div className='opacity-50'>{author.name}</div>
+									<span className='ml-[1px] pr-[3px] opacity-50'>
+										{index < release.author.length - 1 && ','}
+									</span>
+								</div>
+							))}
+						</div>
+					</div>
+
+					<div className='flex shrink-0 gap-[5px] items-center'>
+						<ReleaseRatings
+							ratings={release.ratings}
+							className={'size-9 2xl:size-11 text-base'}
+						/>
+					</div>
+				</div>
+
+				<div className='mt-auto flex items-center justify-between w-full h-10 2xl:h-11'>
+					<button
+						onClick={() => {
+							navigateToRelease(release.id)
+						}}
+						className='h-full text-white flex items-center justify-center border border-white bg-black hover:text-black hover:bg-white cursor-pointer transition-colors duration-200 rounded-full w-[65%] text-xs 2xl:text-base font-semibold'
+					>
+						Написать рецензию...
+					</button>
+
+					<button
+						onClick={() => {
+							navigateToRelease(release.id)
+						}}
+						className='border border-white/0 px-3 h-full bg-white/10 flex items-center rounded-full min-w-20 lg:min-w-25 justify-center text-center gap-1 hover:bg-white/15 hover:border-white/10 cursor-pointer transition-colors duration-200'
+					>
+						<TextReviewSvgIcon classname={'size-5 mr-1'} />
+						<span>{release.text_count + release.no_text_count}</span>
+					</button>
+				</div>
+			</div>
+		)
+	)
+}
+
+export default MostReviewedCarouselCard
---
+++ client/src/pages/main-page/ui/most-reviewed-releases/swiper/Most-reviewed-swiper.tsx
@@ -0,0 +1,93 @@
+import { FC } from 'react'
+import { CloseSvgIcon } from '../../../../../components/notifications/NotificationSvgIcons'
+import {
+	NextSvgIcon,
+	PrevSvgIcon,
+} from '../../../../../components/svg/ReleaseSvgIcons'
+import { useStore } from '../../../../../hooks/UseStore'
+import MostReviewedSwiperButton from './Most-reviewed-swiper-button'
+import MostReviewedCarouselCard from './Most-reviewed-swiper-card'
+
+interface IProps {
+	show: boolean
+	setShow: (val: boolean) => void
+	index: number
+	setIndex: (val: number) => void
+}
+
+const MostReviewedSwiper: FC<IProps> = ({ show, setShow, index, setIndex }) => {
+	const { releasesStore } = useStore()
+
+	return (
+		<div
+			className={`fixed inset-0 z-[1000000] backdrop-blur-3xl overflow-y-auto h-[100vh] flex items-center justify-center transition-all duration-200 gap-3 ${
+				show
+					? 'opacity-100 pointer-events-auto'
+					: 'opacity-0 pointer-events-none'
+			}`}
+		>
+			<span
+				onClick={() => {
+					setShow(false)
+					setIndex(0)
+				}}
+			>
+				<CloseSvgIcon
+					classname={
+						'absolute right-1 top-2.5 lg:right-5 lg:top-5 size-8 lg:size-10 bg-zinc-950 rounded-full flex items-center justify-center border border-white/10 cursor-pointer p-2'
+					}
+				/>
+			</span>
+
+			{index > 0 && (
+				<div
+					key={`prev-${index}`}
+					className='hidden absolute left-[calc(50%-620px)] transform -translate-y-1/2 top-1/2 blur-xs h-full xl:flex items-center scale-90 slide-in-top'
+				>
+					<MostReviewedCarouselCard
+						release={releasesStore.topReleases[index - 1]}
+						index={index - 1}
+					/>
+				</div>
+			)}
+
+			<MostReviewedSwiperButton
+				disabled={index === 0}
+				onClick={() => setIndex(index - 1)}
+			>
+				<PrevSvgIcon />
+			</MostReviewedSwiperButton>
+
+			<div
+				key={index}
+				className='relative slide-in-top h-full flex items-center justify-center'
+			>
+				<MostReviewedCarouselCard
+					release={releasesStore.topReleases[index]}
+					index={index}
+				/>
+			</div>
+
+			<MostReviewedSwiperButton
+				disabled={index === releasesStore.topReleases.length - 1}
+				onClick={() => setIndex(index + 1)}
+			>
+				<NextSvgIcon />
+			</MostReviewedSwiperButton>
+
+			{index + 1 < releasesStore.topReleases.length && (
+				<div
+					key={`next-${index}`}
+					className='hidden absolute right-[calc(50%-620px)] transform -translate-y-1/2 top-1/2 blur-xs h-full xl:flex items-center scale-90 slide-in-top'
+				>
+					<MostReviewedCarouselCard
+						release={releasesStore.topReleases[index + 1]}
+						index={index + 1}
+					/>
+				</div>
+			)}
+		</div>
+	)
+}
+
+export default MostReviewedSwiper
