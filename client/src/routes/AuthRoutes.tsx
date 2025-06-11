7ec2dda9191dcca46219b78d6c5a183cd1ef8567
Daniil Khlyshchankou <gfhieemobilelegends@gmail.com>
2025-06-11 14:26:25 +0300

    Refactored: request reset password form

--- client/src/routes/AuthRoutes.tsx
+++ client/src/routes/AuthRoutes.tsx
@@ -1,9 +1,9 @@
 import { Route, Routes } from 'react-router'
-import ReqResetPasswordForm from '../components/auth/forms/ReqResetPasswordForm'
 import ResetPasswordForm from '../components/auth/forms/ResetPasswordForm'
 import ActivationForm from '../pages/auth-page/ui/forms/Activation-form'
 import LoginForm from '../pages/auth-page/ui/forms/Login-form'
 import RegistrationForm from '../pages/auth-page/ui/forms/Registration-form'
+import ReqResetPasswordForm from '../pages/auth-page/ui/forms/Req-reset-password-form'
 import { ROUTES } from './Routes'
 
 const AuthRoutes = () => {
