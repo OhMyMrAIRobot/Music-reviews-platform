16ffba8be7e5db16ff24ea0e33356c65ca2a972a
Daniil Khlyshchankou <gfhieemobilelegends@gmail.com>
2025-06-15 14:08:09 +0300

    Refactored: footer

--- client/src/components/profile/SocialIcon.tsx
+++ client/src/components/profile/SocialIcon.tsx
@@ -1,21 +1,19 @@
 import { SocialMediaEnum } from '../../models/profile/social-media-enum'
-import {
-	TelegramSvgIcon,
-	TwitchSvgIcon,
-	VKSvgIcon,
-	YoutubeSvgIcon,
-} from './SocialSvgItems'
+import TelegramSvg from '../svg/Telegram-svg'
+import TwitchSvg from '../svg/Twitch-svg'
+import VkSvg from '../svg/Vk-svg'
+import YouTubeSvg from '../svg/YouTube-svg'
 
 const SocialIcon = ({ name }: { name: string }) => {
 	switch (name) {
 		case SocialMediaEnum.TELEGRAM:
-			return <TelegramSvgIcon />
+			return <TelegramSvg className={'size-5'} />
 		case SocialMediaEnum.TWITCH:
-			return <TwitchSvgIcon />
+			return <TwitchSvg className={'size-5'} />
 		case SocialMediaEnum.VK:
-			return <VKSvgIcon />
+			return <VkSvg className={'size-5'} />
 		case SocialMediaEnum.YOUTUBE:
-			return <YoutubeSvgIcon />
+			return <YouTubeSvg className={'size-5'} />
 		default:
 			return null
 	}
