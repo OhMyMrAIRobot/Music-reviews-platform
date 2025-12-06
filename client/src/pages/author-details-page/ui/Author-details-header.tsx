514bf8aa06323fa26afdffec6dd40014281fb292
Daniil Khlyshchankou <gfhieemobilelegends@gmail.com>
2025-12-06 23:11:43 +0300

    feat: implement favorite author toggle functionality with query invalidation and error handling

--- client/src/pages/author-details-page/ui/Author-details-header.tsx
+++ client/src/pages/author-details-page/ui/Author-details-header.tsx
@@ -1,11 +1,23 @@
-import { FC, useEffect } from 'react'
+import {
+	InvalidateQueryFilters,
+	useMutation,
+	useQueryClient,
+} from '@tanstack/react-query'
+import { observer } from 'mobx-react-lite'
+import { FC } from 'react'
+import { UserFavAuthorAPI } from '../../../api/author/user-fav-author-api'
 import AuthorNominations from '../../../components/author/author-nomination/Author-nominations'
 import AuthorTypes from '../../../components/author/author-types/Author-types'
 import RegisteredAuthorTypes from '../../../components/author/registered-author/Registered-author-types'
 import ToggleFavButton from '../../../components/buttons/Toggle-fav-button'
 import LikesCount from '../../../components/utils/Likes-count'
 import SkeletonLoader from '../../../components/utils/Skeleton-loader'
+import { useApiErrorHandler } from '../../../hooks/use-api-error-handler'
+import { useAuth } from '../../../hooks/use-auth'
 import { useStore } from '../../../hooks/use-store'
+import { authorsKeys } from '../../../query-keys/authors-keys'
+import { leaderboardKeys } from '../../../query-keys/leaderboard-keys'
+import { profilesKeys } from '../../../query-keys/profiles-keys'
 import { Author } from '../../../types/author'
 
 interface IProps {
@@ -13,45 +25,67 @@ interface IProps {
 	isLoading: boolean
 }
 
-const AuthorDetailsHeader: FC<IProps> = ({ author, isLoading }) => {
-	const { authStore } = useStore()
+const AuthorDetailsHeader: FC<IProps> = observer(({ author, isLoading }) => {
+	/** HOOKS */
+	const { authStore, notificationStore } = useStore()
+	const { checkAuth } = useAuth()
+	const handleApiError = useApiErrorHandler()
+	const queryClient = useQueryClient()
 
-	// const [toggling, setToggling] = useState(false)
-	// const { storeToggle } = useQueryListFavToggleAll<IAuthor, IAuthor>(
-	// 	authorsKeys.all,
-	// 	null,
-	// 	toggleFavAuthor
-	// )
-
-	const isFav =
+	let isFav =
 		author?.userFavAuthor?.some(val => val.userId === authStore.user?.id) ??
 		false
 
-	// const toggle = async () => {
-	// 	if (!checkAuth() || !author) {
-	// 		return
-	// 	}
-	// 	setToggling(true)
+	/**
+	 * Function to invalidate related queries after mutations
+	 */
+	const invalidateRelatedQueries = () => {
+		const keysToInvalidate: InvalidateQueryFilters[] = [
+			{ queryKey: profilesKeys.profile(authStore.user?.id ?? 'unknown') },
+			{ queryKey: profilesKeys.preferences(authStore.user?.id ?? 'unknown') },
+			{ queryKey: authorsKeys.all },
+			{ queryKey: leaderboardKeys.all },
+		]
 
-	// 	const errors = await storeToggle(author.id, isFav)
+		keysToInvalidate.forEach(key => queryClient.invalidateQueries(key))
+	}
 
-	// 	if (errors.length === 0) {
-	// 		notificationStore.addSuccessNotification(
-	// 			isFav
-	// 				? 'Вы убрали автора из списка понравившихся'
-	// 				: 'Вы добавили автора в список понравившихся!'
-	// 		)
-	// 	} else {
-	// 		errors.forEach((err: string) =>
-	// 			notificationStore.addErrorNotification(err)
-	// 		)
-	// 	}
-	// 	setToggling(false)
-	// }
+	/**
+	 * Mutation to toggle favorite author
+	 */
+	const toggleFavMutation = useMutation({
+		mutationFn: (id: string) =>
+			isFav
+				? UserFavAuthorAPI.deleteFromFav(id)
+				: UserFavAuthorAPI.addToFav(id),
+		onSuccess: () => {
+			notificationStore.addSuccessNotification(
+				isFav
+					? 'Автор успешно удален из избранного!'
+					: 'Автор успешно добавлен в избранное!'
+			)
 
-	useEffect(() => {
-		console.log(author)
-	}, [author])
+			isFav = !isFav
+			invalidateRelatedQueries()
+		},
+		onError: (error: unknown) => {
+			handleApiError(
+				error,
+				isFav
+					? 'Не удалось убрать автора из избранного!'
+					: 'Не удалось добавить автора в избранное!'
+			)
+		},
+	})
+
+	/**
+	 * Function to toggle favorite author
+	 */
+	const toggleFav = () => {
+		if (!checkAuth() || !author || toggleFavMutation.isPending) return
+
+		return toggleFavMutation.mutate(author.id)
+	}
 
 	return isLoading ? (
 		<SkeletonLoader className='relative w-full h-45 md:h-62 lg:h-125 rounded-2xl overflow-hidden' />
@@ -91,10 +125,10 @@ const AuthorDetailsHeader: FC<IProps> = ({ author, isLoading }) => {
 						)}
 
 						<ToggleFavButton
-							onClick={() => {}} // todo: fix toggle
+							onClick={toggleFav}
 							isLiked={isFav}
 							className='absolute top-3 right-3 z-300 size-10 lg:size-12'
-							toggling={false}
+							toggling={toggleFavMutation.isPending}
 						/>
 
 						<div className='flex absolute bottom-5 lg:bottom-10 gap-3 z-300 w-full px-4 lg:px-10'>
@@ -135,6 +169,6 @@ const AuthorDetailsHeader: FC<IProps> = ({ author, isLoading }) => {
 			</section>
 		)
 	)
-}
+})
 
 export default AuthorDetailsHeader
