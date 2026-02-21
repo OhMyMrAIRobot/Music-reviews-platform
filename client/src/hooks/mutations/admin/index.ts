867bb242018aa26f711c8836315c78fa607a7d5c
Daniil Khlyshchankou <gfhieemobilelegends@gmail.com>
2026-02-22 01:09:07 +0300

    feat: add useAdminUpdateAuthorCommentMutation hook and integrate it into AuthorCommentFormModal

--- client/src/hooks/mutations/admin/index.ts
+++ client/src/hooks/mutations/admin/index.ts
@@ -1,1 +1,2 @@
 export * from './use-admin-remove-author-comment-mutation'
+export * from './use-admin-update-author-comment-mutation'
---
+++ client/src/hooks/mutations/admin/use-admin-update-author-comment-mutation.ts
@@ -0,0 +1,48 @@
+import {
+	InvalidateQueryFilters,
+	useMutation,
+	useQueryClient,
+} from '@tanstack/react-query'
+import { AuthorCommentAPI } from '../../../api/author/author-comment-api'
+import { authorCommentsKeys } from '../../../query-keys/author-comments-keys'
+import { UpdateAuthorCommentData } from '../../../types/author'
+import { UseMutationParams } from '../../../types/common'
+import { useApiErrorHandler } from '../../use-api-error-handler'
+import { useStore } from '../../use-store'
+
+export const useAdminUpdateAuthorCommentMutation = ({
+	onSuccess,
+	onError,
+	onSettled,
+}: UseMutationParams = {}) => {
+	const { notificationStore } = useStore()
+	const handleApiError = useApiErrorHandler()
+	const queryClient = useQueryClient()
+
+	const invalidateRelatedQueries = () => {
+		const keysToInvalidate: InvalidateQueryFilters[] = [
+			{ queryKey: authorCommentsKeys.all },
+		]
+
+		keysToInvalidate.forEach(key => queryClient.invalidateQueries(key))
+	}
+
+	const mutation = useMutation({
+		mutationFn: ({ id, data }: { id: string; data: UpdateAuthorCommentData }) =>
+			AuthorCommentAPI.adminUpdate(id, data),
+		onSuccess: () => {
+			notificationStore.addSuccessNotification(
+				'Авторский комментарий успешно обновлен!',
+			)
+			invalidateRelatedQueries()
+			onSuccess?.()
+		},
+		onError: (error: unknown) => {
+			handleApiError(error, 'Не удалось обновить авторский комментарий!')
+			onError?.(error)
+		},
+		onSettled,
+	})
+
+	return mutation
+}
