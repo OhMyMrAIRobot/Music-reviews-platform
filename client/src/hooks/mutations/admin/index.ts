b40251355a81acf2112d61aef16288e1412dbe2d
Daniil Khlyshchankou <gfhieemobilelegends@gmail.com>
2026-02-22 18:00:14 +0300

    feat: add useAdminRemoveAuthorMutation hook and integrate it into AdminDashboardAuthorsGridItem

--- client/src/hooks/mutations/admin/index.ts
+++ client/src/hooks/mutations/admin/index.ts
@@ -1,4 +1,5 @@
 export * from './use-admin-remove-author-comment-mutation'
 export * from './use-admin-remove-author-confirmation-mutation'
+export * from './use-admin-remove-author-mutation'
 export * from './use-admin-update-author-comment-mutation'
 export * from './use-admin-update-author-confirmation-mutation'
