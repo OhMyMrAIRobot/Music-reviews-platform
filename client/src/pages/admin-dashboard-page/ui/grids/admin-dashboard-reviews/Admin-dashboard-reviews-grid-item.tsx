0af16cf19b8697418654ebce6493fd435239c030
Daniil Khlyshchankou <gfhieemobilelegends@gmail.com>
2026-02-24 23:41:07 +0300

    feat: implement useAdminRemoveReviewMutation hook and integrate into AdminDashboardReviewsGridItem

--- client/src/pages/admin-dashboard-page/ui/grids/admin-dashboard-reviews/Admin-dashboard-reviews-grid-item.tsx
+++ client/src/pages/admin-dashboard-page/ui/grids/admin-dashboard-reviews/Admin-dashboard-reviews-grid-item.tsx
@@ -1,25 +1,10 @@
-import {
-	InvalidateQueryFilters,
-	useMutation,
-	useQueryClient,
-} from '@tanstack/react-query'
 import { FC, useState } from 'react'
 import { Link } from 'react-router'
-import { ReviewAPI } from '../../../../../api/review/review-api.ts'
 import ArrowBottomSvg from '../../../../../components/layout/header/svg/Arrow-bottom-svg.tsx'
 import ConfirmationModal from '../../../../../components/modals/Confirmation-modal.tsx'
 import SkeletonLoader from '../../../../../components/utils/Skeleton-loader.tsx'
-import { useApiErrorHandler } from '../../../../../hooks/use-api-error-handler.ts'
+import { useAdminRemoveReviewMutation } from '../../../../../hooks/mutations/index.ts'
 import useNavigationPath from '../../../../../hooks/use-navigation-path.ts'
-import { useStore } from '../../../../../hooks/use-store.ts'
-import { authorsKeys } from '../../../../../query-keys/authors-keys.ts'
-import { leaderboardKeys } from '../../../../../query-keys/leaderboard-keys.ts'
-import { platformStatsKeys } from '../../../../../query-keys/platform-stats-keys.ts'
-import { profilesKeys } from '../../../../../query-keys/profiles-keys.ts'
-import { releaseMediaKeys } from '../../../../../query-keys/release-media-keys.ts'
-import { releasesKeys } from '../../../../../query-keys/releases-keys.ts'
-import { reviewsKeys } from '../../../../../query-keys/reviews-keys.ts'
-import { usersKeys } from '../../../../../query-keys/users-keys.ts'
 import { SortOrdersEnum } from '../../../../../types/common/enums/sort-orders-enum.ts'
 import { SortOrder } from '../../../../../types/common/types/sort-order.ts'
 import { Review } from '../../../../../types/review/index.ts'
@@ -44,46 +29,13 @@ const AdminDashboardReviewsGridItem: FC<IProps> = ({
 	order,
 	toggleOrder,
 }) => {
-	/** HOOKS */
-	const { notificationStore } = useStore()
-	const handleApiError = useApiErrorHandler()
-	const queryClient = useQueryClient()
 	const { navigateToReleaseDetails, navigatoToProfile } = useNavigationPath()
 
-	/** STATES */
 	const [confModalOpen, setConfModalOpen] = useState<boolean>(false)
 	const [editModalOpen, setEditModalOpen] = useState<boolean>(false)
 
-	/**
-	 * Function to invalidate related queries after mutations
-	 */
-	const invalidateRelatedQueries = () => {
-		const keysToInvalidate: InvalidateQueryFilters[] = [
-			{ queryKey: profilesKeys.all },
-			{ queryKey: leaderboardKeys.all },
-			{ queryKey: releasesKeys.all },
-			{ queryKey: reviewsKeys.all },
-			{ queryKey: usersKeys.all },
-			{ queryKey: platformStatsKeys.all },
-			{ queryKey: authorsKeys.all },
-			{ queryKey: releaseMediaKeys.all },
-		]
-
-		keysToInvalidate.forEach(key => queryClient.invalidateQueries(key))
-	}
-
-	/**
-	 * Mutation to delete the review
-	 */
-	const deleteMutation = useMutation({
-		mutationFn: ({ id }: { id: string }) => ReviewAPI.adminDelete(id),
-		onSuccess: () => {
-			notificationStore.addSuccessNotification('Вы успешно удалили рецензию!')
-			setConfModalOpen(false)
-			invalidateRelatedQueries()
-		},
-		onError: (error: unknown) => {
-			handleApiError(error, 'Не удалось удалить рецензию')
+	const { mutateAsync, isPending } = useAdminRemoveReviewMutation({
+		onSettled: () => {
 			setConfModalOpen(false)
 		},
 	})
@@ -98,9 +50,9 @@ const AdminDashboardReviewsGridItem: FC<IProps> = ({
 						<ConfirmationModal
 							title={'Вы действительно хотите удалить рецензию?'}
 							isOpen={confModalOpen}
-							onConfirm={() => deleteMutation.mutate({ id: review.id })}
+							onConfirm={() => mutateAsync({ id: review.id })}
 							onCancel={() => setConfModalOpen(false)}
-							isLoading={deleteMutation.isPending}
+							isLoading={isPending}
 						/>
 					)}
 
