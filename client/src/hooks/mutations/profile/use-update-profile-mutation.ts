ae3b4bc354df11903d1d471e498cf8f54b827b8e
Daniil Khlyshchankou <gfhieemobilelegends@gmail.com>
2026-03-02 20:51:14 +0300

    refactor: simplify UploadAvatarForm by utilizing useUpdateProfileMutation and removing redundant code

--- client/src/hooks/mutations/profile/use-update-profile-mutation.ts
+++ client/src/hooks/mutations/profile/use-update-profile-mutation.ts
@@ -4,7 +4,12 @@ import {
 	useQueryClient,
 } from '@tanstack/react-query'
 import { ProfileAPI } from '../../../api/user/profile-api'
+import { authorCommentsKeys } from '../../../query-keys/author-comments-keys'
+import { authorLikesKeys } from '../../../query-keys/author-likes-keys'
+import { leaderboardKeys } from '../../../query-keys/leaderboard-keys'
 import { profilesKeys } from '../../../query-keys/profiles-keys'
+import { releaseMediaKeys } from '../../../query-keys/release-media-keys'
+import { reviewsKeys } from '../../../query-keys/reviews-keys'
 import { usersKeys } from '../../../query-keys/users-keys'
 import { UseMutationParams } from '../../../types/common'
 import { useApiErrorHandler } from '../../use-api-error-handler'
@@ -30,24 +35,29 @@ export const useUpdateProfileMutation = ({
 	const invalidateRelatedQueries = (userId: string) => {
 		const keysToInvalidate: InvalidateQueryFilters[] = [
 			{ queryKey: profilesKeys.profile(userId) },
+			{ queryKey: leaderboardKeys.all },
+			{ queryKey: authorCommentsKeys.all },
+			{ queryKey: authorLikesKeys.all },
+			{ queryKey: releaseMediaKeys.all },
+			{ queryKey: reviewsKeys.all },
 			{ queryKey: usersKeys.all },
 		]
 
 		keysToInvalidate.forEach(key => queryClient.invalidateQueries(key))
 	}
+
 	const mutation = useMutation({
 		mutationFn: (formData: FormData) => ProfileAPI.update(formData),
-		onSuccess: () => {
+		onSuccess: data => {
 			if (authStore.user?.id) {
 				invalidateRelatedQueries(authStore.user.id)
 			}
-			notificationStore.addSuccessNotification(
-				'Описание профиля успешно обновлено!',
-			)
+			authStore.setProfile(data)
+			notificationStore.addSuccessNotification('Профиль успешно обновлен!')
 			onSuccess?.()
 		},
 		onError: (error: unknown) => {
-			handleApiError(error, 'Ошибка при обновлении описания профиля!')
+			handleApiError(error, 'Ошибка при обновлении профиля!')
 			onError?.(error)
 		},
 		onSettled,
